bef6dc3250287f0747f1f8d0358ad4e0f12dafaa
Jon-Morten Kristiansen <jon.morten.kristiansen@gmail.com>
2014-03-27 08:36:53 +0100

    Add commands skeleton

---
+++ PHPNaHa.py
@@ -0,0 +1,35 @@
+import sublime
+import sublime_plugin
+
+import os
+
+
+class PhpnahaIndexProjectNamespaces(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        print('Hello')
+        pass
+
+
+class PhpnahaInsertNamespaceStatement(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        pass
+
+
+class PhpnahaCopyNamespaceAndClass(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        pass
+
+
+class PhpnahaOpenClassFile(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        pass
+
+
+class PhpnahaFindClassAndInsertUseStatement(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        pass
