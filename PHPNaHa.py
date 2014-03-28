1aee6b84dbca59fa248172a4884b0efeb5558a15
Jon-Morten Kristiansen <jon.morten.kristiansen@gmail.com>
2014-03-28 10:48:16 +0100

    Open and find: Arrow and double colon operators case

--- PHPNaHa.py
+++ PHPNaHa.py
@@ -269,6 +269,10 @@ class PhpnahaOpenClassFile(sublime_plugin.TextCommand, FilePreviewer):
             word_region = self._current_view.expand_by_class(region, sublime.CLASS_WORD_START | sublime.CLASS_WORD_END | sublime.CLASS_LINE_START | sublime.CLASS_LINE_END, ' ')
             word = self._current_view.substr(word_region).strip().strip('\\')
             word = re.sub(r'^[^\w]+|[^\w]+$', '', str(word))
+            if '->' in word:
+                word = next(iter(word.split('->')), False)
+            if '::' in word:
+                word = next(iter(word.split('::')), False)
             if word:
                 # Check if word is
                 # 1. imported with use
@@ -320,6 +324,10 @@ class PhpnahaFindClassAndInsertUseStatement(sublime_plugin.TextCommand, FilePrev
             word_region = self._current_view.expand_by_class(region, sublime.CLASS_WORD_START | sublime.CLASS_WORD_END | sublime.CLASS_LINE_START | sublime.CLASS_LINE_END)
             word = self._current_view.substr(word_region).strip().strip('\\')
             word = re.sub(r'^[^\w]+|[^\w]+$', '', str(word))
+            if '->' in word:
+                word = next(iter(word.split('->')), False)
+            if '::' in word:
+                word = next(iter(word.split('::')), False)
             if word:
                 namespace_region = self._current_view.find(r'namespace ([^ ;]+)', 0)
                 if namespace_region:
